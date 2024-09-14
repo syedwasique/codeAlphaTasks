1a2b5e1dbdb3ca3f96c17ab0de39990e21ec2f08
syedwasique <140272288+syedwasique@users.noreply.github.com>
2024-09-14 20:49:39 +0500

    Add files via upload

---
+++ ImageGallery/index.js
@@ -0,0 +1,18 @@
+const images = ["img/img1.jpg", "img/image2.jpg", "img/img3.jpg", "img/img4.jpg"];
+let currentIndex = 0;
+
+function showImage(index) {
+    document.getElementById('currentImage').src = images[index];
+    currentIndex = index;
+}
+
+
+function nextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(currentIndex);
+}
+
+function prevImage() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    showImage(currentIndex);
+}
